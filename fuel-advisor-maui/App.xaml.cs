d19f0f62388a31cea501d76da1958739bdc3bd3f
Brennan-M-Long <brennanmlong@outlook.com>
2025-02-11 06:54:42 -0500

    @Josh - large commit:
    [ x ] - Added .NET MAUI "fuel-advisor-maui".cs; .csproj; sln
    
    [ x ] - Added .NET Maui reference to Fuel-Advisor.sln (main sln)
    
    [ x ] - Accepted Android license; ensured Android, iOS, MAC Catalyst, Windows license were up-to-date
    
    [ x ] - Updated README.md with GitHub Branch Naming Conventions documentation
    
    [   ] - @Josh verify the following were done correctly before merging with main

---
+++ fuel-advisor-maui/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿namespace fuel_advisor_maui;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+	}
+
+	protected override Window CreateWindow(IActivationState? activationState)
+	{
+		return new Window(new AppShell());
+	}
+}
